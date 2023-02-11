692e6225320ad38fef14fd975f8c7a5d8f296292
Roxanny Rodrigues <117939674+RoxannyRodrigues@users.noreply.github.com>
2023-02-11 17:48:02 -0300

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -12,9 +12,15 @@ print("Conectado")
 
 
 
-print("Bem-Vindo")
+print(f'''Bem-Vindo
+Escolha:
+1.Consultar
+2.Cadastros ou Atualização
+3.Alugueis''')
 
-def menuLivros ():
+
+
+def menuconsultarLivros ():
 
     print(f'''
     Selecione o que deseja fazer:
@@ -49,6 +55,43 @@ def menuLivros ():
 
 
 
+def menuconsultarClientes ():
+
+    print(f'''
+    Selecione o que deseja fazer:
+    1. Buscar Cliente por ID
+    2. Buscar Cleinte por CPF
+    ''')
+    escolha = input("Digite: ")
+    match escolha:
+        case "1":
+            iddocliente = input("Digite o ID: ")
+            sqlcliente = f'''SELECT * FROM cliente
+                        WHERE ID = {iddocliente} '''
+            cursor.execute(sqlcliente)            
+            resultadocliente = cursor.fetchall()
+            cursor.close()
+            conexao.close()
+
+            for client in resultadocliente:
+                print(f"ID: {client[0]} - Nome: {client[1]} - CPF: {client[2]} - Limite de Livros: {client[3]} \n")
+        
+        case "2":
+            cpfdocliente = input("Digite o CPF: ")
+            sqlcliente = f'''SELECT * FROM cliente
+                        WHERE cpf = {cpfdocliente}'''
+            cursor.execute(sqlcliente)            
+            resultadocpfcliente = cursor.fetchall()
+            cursor.close()
+            conexao.close()
+
+            for cpfcliente in resultadocpfcliente:
+                print(f"ID: {cpfcliente[0]} - Nome: {cpfcliente[1]} - CPF: {cpfcliente[2]} - Limite de Livros: {cpfcliente[3]} \n")
 
 
-teste = menuLivros()
+
+menuconsultarClientes()
+
+
+
+             
